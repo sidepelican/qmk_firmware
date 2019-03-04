a4cfa454d2f149bc78ab4ba8c0adf23747de3605
kenta_okamura <side.junktown@gmail.com>
2019-03-05 00:41:33 +0900

    set enter key at right edge

--- keyboards/ergodash/rev2/keymaps/iceman/keymap.c
+++ keyboards/ergodash/rev2/keymaps/iceman/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UP  ,                        KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LALT, KC_LGUI, EISU,             LOWER,   KC_SPC ,KC_DOWN,        KC_BSPC,KC_ENT , RAISE,            KANA,    KC_RGUI, KC_DOWN, KC_UP    \
+    KC_ESC,  KC_LALT, KC_LGUI, EISU,             LOWER,   KC_SPC ,KC_DOWN,        KC_BSPC,KC_ENT , RAISE,            KANA,    KC_RGUI, KC_LBRC, KC_RBRC  \
   ),
 
   /* Lower
