1f5f0b82b396fc20dbf717657a4266ebc3347347
kenta_okamura <side.junktown@gmail.com>
2019-03-05 23:26:43 +0900

    shift arrow keys and LGUI

--- keyboards/ergodash/rev2/keymaps/iceman/keymap.c
+++ keyboards/ergodash/rev2/keymaps/iceman/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UP  ,                        KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LALT, KC_LGUI, EISU,             LOWER,   KC_SPC ,KC_DOWN,        KC_BSPC,KC_ENT , RAISE,            KANA,    KC_RGUI, KC_LBRC, KC_RBRC  \
+    KC_ESC,  KC_LALT, KC_LALT, KC_LGUI,          LOWER,   KC_SPC ,KC_DOWN,        KC_BSPC,KC_ENT , RAISE,            KANA,    KC_RGUI, KC_LBRC, KC_RBRC  \
   ),
 
   /* Lower
@@ -81,8 +81,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-    _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, KC_UP,   _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,                        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, KC_UP,   _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, \
     _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______,          LOWER,   _______,_______,        _______,_______, RAISE,            _______, _______, KC_PGDN, KC_PGUP  \
   ),
