3120fb42b9dc74b1408004a4a3c29a13249faadc
kenta-okamura <kenta-okamura@cookpad.com>
2019-06-20 14:53:21 +0900

    fix ergodash cannot copile and fix layout

--- keyboards/ergodash/rev1/keymaps/iceman/keymap.c
+++ keyboards/ergodash/rev1/keymaps/iceman/keymap.c
@@ -1,6 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "action_layer.h"
-#include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
 
@@ -11,16 +9,15 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
+  LOWER = _LOWER,
+  RAISE = _RAISE,
+  ADJUST = _ADJUST,
 };
 
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
 #define EISU KC_LANG2
 #define KANA KC_LANG1
 #define KC_RSENT LT(RAISE, KC_ENT)
+#define KC_FXIS C(A(LCMD(KC_F)))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -40,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ENT,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSLS,                        KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOT,                        KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,                        KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UP  ,                        KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LALT, KC_LALT, KC_LGUI,   LT(LOWER,EISU), KC_SPC ,KC_DOWN,        KC_BSPC,KC_RSENT,LT(RAISE, KANA),  KC_RGUI, KC_SPC,  KC_MINS, KC_EQL   \
+    KC_ESC,  KC_LALT, KC_LALT, KC_LGUI,   LT(LOWER,EISU), KC_SPC ,KC_DOWN,        KC_BSPC,KC_RSENT,LT(RAISE, KANA),  KC_RGUI, KC_SPC,  KC_FXIS, KC_ENT   \
   ),
 
   /* Lower
@@ -60,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______,                        _______, KC_7,   KC_8,    KC_9,    _______, _______, _______, \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,                        KC_6   , KC_7,   KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, \
-    _______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, _______, _______,                        _______, KC_4,   KC_5,    KC_6,    _______, KC_QUOT, _______, \
-    _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, KC_PGUP,                        KC_0   , KC_1,   KC_2,    KC_3,    KC_DOT,  _______, _______, \
-    _______, _______, _______, _______,          LOWER,   _______,KC_PGDN,        _______,_______, RAISE,           _______, _______, KC_LEFT, KC_RGHT  \
+    _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______, \
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,                        KC_6   , KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, \
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, \
+    _______, _______, _______, _______, _______, _______, KC_PGUP,                        _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______,          LOWER  , _______,KC_PGDN,        _______,_______, RAISE  ,          _______, _______, KC_LEFT, KC_RGHT  \
   ),
 
   /* Raise
@@ -85,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, KC_UP,   KC_LBRC, KC_RBRC, _______, \
     _______, _______, _______, _______, _______, _______, _______,                        _______, _______, KC_LEFT, KC_RGHT, XXXXXXX, _______, _______, \
     _______, _______, _______, _______, _______, _______, KC_PGUP,                        _______, _______, KC_DOWN, XXXXXXX, _______, _______, _______, \
-    _______, _______, _______, _______,          LOWER,   _______,KC_PGDN,        _______,_______, RAISE,            _______, _______, _______, _______  \
+    _______, _______, _______, _______,          LOWER  , _______,KC_PGDN,        _______,_______, RAISE  ,          _______, _______, _______, _______  \
   ),
 
   /* Adjust
